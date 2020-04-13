db7d4fbea90aa972656065458bc1109905f278ff
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2020-04-12 23:49:51 -0500

    Added Maximum_Subarray_Sum

---
+++ maximum_subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int local_max =0;
+        int global_max = INT_MIN;
+        //Kadane's Algorithm
+        for(int i =0,n = nums.size();i<n;i++)
+        {
+            local_max = max(nums[i],nums[i]+local_max);
+            if(local_max>global_max)
+            {
+                global_max = local_max;
+            }
+        }
+        return global_max;
+    }
+};
