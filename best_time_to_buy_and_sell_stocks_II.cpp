eb43a1c0d688efdd44204460c0754d3bd0f21758
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2020-04-16 19:33:01 -0500

    Added Best Time to Buy and Sell Stocks II

---
+++ best_time_to_buy_and_sell_stocks_II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        for(int i =1;i<prices.size();i++)
+        {
+            //Peak-valley pattern approach
+            if(prices[i]>prices[i-1])
+                max_profit += prices[i]-prices[i-1];
+        }
+        return max_profit;
+    }
+};
