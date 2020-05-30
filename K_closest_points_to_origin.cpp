70db1440b5dfc73268c77043388e17e6c549f295
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2020-05-30 03:50:00 -0500

    Revised K closest

--- K_closest_points_to_origin.cpp
+++ K_closest_points_to_origin.cpp
@@ -1,20 +1,16 @@
+// Ref : BarackGrande@Disc
 class Solution {
 public:
-// Ref : BarackGrande@Disc
-
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
         //Optimization code
         cin.tie(NULL);
         ios_base::sync_with_stdio(0);
         
-        //Custom comparison function
-        auto distanceSort = []( vector<int> &a, vector<int> &b)
+       //Sort by distance in ascending order using custom comparator
+nth_element(points.begin(),points.begin()+K, points.end(),[]( vector<int> &a, vector<int> &b)
         {
         return (a[0] * a[0] + a[1] * a[1]) < (b[0]*b[0] + b[1]*b[1]);    
-        };
-        
-       //Sort by distance in ascending order
-nth_element(points.begin(),points.begin()+K, points.end(),distanceSort);
+        });
         //Resize the points vector to K elements
         points.resize(K);
         return points;
